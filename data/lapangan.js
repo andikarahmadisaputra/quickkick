917abfe30f7250d3e2852cba5bdc37f063099704
Andika Rahmadi Saputra <andikars811@gmail.com>
2025-03-01 22:49:39 +0700

    buat data dummy

---
+++ data/lapangan.js
@@ -0,0 +1,38 @@
+export const lapangan = [
+  {
+    id: 1,
+    nama: "Lapangan Sintetis A",
+    jenis: "Rumput Sintetis",
+    harga: {
+      reguler: 120000,
+      weekend: 150000,
+      promo: {
+        reguler: 100000,
+        weekend: 130000,
+      },
+    },
+    fasilitas: ["Lampu LED", "Bench Pemain", "Toilet", "Kantin"],
+  },
+  {
+    id: 2,
+    nama: "Lapangan Vinyl B",
+    jenis: "Vinyl",
+    harga: {
+      reguler: 110000,
+      weekend: 140000,
+      promo: { reguler: 90000, weekend: 120000 },
+    },
+    fasilitas: ["AC", "Lampu LED", "Bench Pemain", "Toilet"],
+  },
+  {
+    id: 3,
+    nama: "Lapangan Parquet C",
+    jenis: "Parquet",
+    harga: {
+      reguler: 100000,
+      weekend: 130000,
+      promo: { reguler: 0, weekend: 0 },
+    },
+    fasilitas: ["Lampu LED", "Bench Pemain", "Toilet", "Kantin"],
+  },
+];
